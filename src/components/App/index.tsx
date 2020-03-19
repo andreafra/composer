89a7665e30b4f093b54b1256409902c88861f7da
chiara011 <36208248+chiara011@users.noreply.github.com>
2020-03-19 15:03:59 +0100

    Added Button component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './style.css'
+import Button from 'components/Button'
+
 
 function App() {
 
+  const printCiao = () => {
+    console.log("Ciao")
+  }
+
   return (
     <div className="App">
-      
+      <Button name="Ciao" action={() => printCiao()}/>
     </div>
   )
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
-import { prependOnceListener } from 'cluster'
 
-function Button() {
+function Button(props: any) {
 
+  function handleClick() {
+    props.action()
+  }
+
+  return (
+  <button onClick={handleClick}>
+    {props.name}
+  </button>
+  )
 }
 
 export default Button
