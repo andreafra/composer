a361dce1054adb1f6f48bafcbaa26825dc0fc2ef
Andrea Franchini <hello@andreafranchini.com>
2020-08-29 11:57:25 +0200

    Working sound arduino compiler

--- src/compilers/arduino_v1.ts
+++ src/compilers/arduino_v1.ts
@@ -1,10 +1,5 @@
 import { ComposerState, EditorOptions, SoundFrame, Channel } from "types";
 
-function initSketch(state: ComposerState) {
-  let options = state.system.editorOptions
-
-}
-
 export default class Compiler {
   options: EditorOptions;
   sound: (SoundFrame | null)[];
@@ -24,7 +19,7 @@ export default class Compiler {
   }
   
   header = () => {
-    return `// 1. FRAME SIZE AND TOTAL LENGTH
+    return `// FRAME SIZE AND TOTAL LENGTH
 #define FRAME_LENGTH_MS ${this.options.resolution}
 #define FILE_LENGTH ${this.options.width / this.options.frameSize}
 `
@@ -33,7 +28,7 @@ export default class Compiler {
   setup = () => {
     return `void setup() {
   // SOUND BUZZER
-  pinMode(SPEAKER_PIN, OUTPUT);
+  pinMode(SOUND_PIN, OUTPUT);
   // SETUP LED, MOTORS
   ${this.getActuatorsSetup()}
 }
@@ -41,21 +36,21 @@ export default class Compiler {
   }
 
   loop = () => {
-    return `// 4. HANDLE UPDATE RATE
+    return `// HANDLE UPDATE RATE
 long lastUpdate = 0;
 int updates = 0;
 
+int soundCounter = 0;
+int soundTicksRemaining = 0;
+
 void loop() {
-  unsigned long currentUpdate = millis();
+  unsigned long currentUpdateMillis = millis();
   
-  if (currentUpdate - lastUpdate > FRAME_LENGTH_MS && updates < FILE_LENGTH) {
-    lastUpdate = currentUpdate;
+  if (currentUpdateMillis - lastUpdate > FRAME_LENGTH_MS && updates < FILE_LENGTH) {
+    lastUpdate = currentUpdateMillis;
     // Every frame, execute this block of code
     
-    // SOUND: Tone - Square Wave
-    if (SOUND_SIZE <= FILE_LENGTH) {
-      tone(SPEAKER_PIN, SOUND[updates], FRAME_LENGTH_MS);
-    }
+    ${this.getSoundLoop()}
     // SETUP LED, MOTORS
     ${this.getActuatorsLoop()}
     updates++;
@@ -66,29 +61,63 @@ void loop() {
 
   // TODO: Fix PIN 9
   speaker = () => {
-    return `// 2. INITIALIZE SPEAKER
-#define SPEAKER_PIN 9
+    return `// INITIALIZE SOUND DEVICE
+#define SOUND_PIN 9
 ${this.generateNotes()}
-#define SOUND_SIZE ${this.sound.length}
 `
   }
 
   generateNotes = () => {
-    let code = "const int SOUND[] = {"
-    this.sound.forEach(value => {
-      if (value === null) code += "0,"
-      else {
-        code += Math.round(value.note.freq).toString() + ","
+    let notes_freq = "const int SOUND_FREQ[] = {" // the frequencies to play
+    let notes_length = "const byte SOUND_DURATION[] = {" // how long each frequency lasts
+    let lastFreq = undefined // the last frequency added
+    let lastFreqQty = 0 // how many identical freqs have been parsed consecutively
+    let intervals = 0 // how many different sequences of freqs are in the array
+    let i = 0
+    while(i <= this.sound.length) {
+      let freq = Math.round(this.sound[i]?.note.freq || 0)
+      if (freq === lastFreq) {
+        lastFreqQty++
+      } else {
+        if (lastFreq !== undefined) {
+          notes_freq += lastFreq.toString() + ","
+          notes_length += lastFreqQty.toString() + ","
+        }
+        lastFreq = freq
+        lastFreqQty = 1
+        intervals++
       }
-    })
-    code += "};"
-    return code
+      i++
+    }
+    notes_freq += "};"
+    notes_length += "};"
+    return notes_freq + "\n" + notes_length + `\n#define SOUND_SIZE ${intervals - 1}`
   }
 
-  getActuatorsDefs = () => {}
+  getSoundLoop = () => {
+    return `// SOUND: Tone - Square Wave
+    if (soundCounter < SOUND_SIZE) {
+      if (soundTicksRemaining == 0) {
+        tone(SOUND_PIN, SOUND_FREQ[soundCounter], SOUND_DURATION[soundCounter] * FRAME_LENGTH_MS);
+        soundTicksRemaining = SOUND_DURATION[soundCounter] - 1;
+        soundCounter++;
+      } else {
+        soundTicksRemaining--;
+      }
+    } else noTone(SOUND_PIN);
+    `
+  }
+  
+  getActuatorsDefs = () => {
+    return ""
+  }
 
-  getActuatorsSetup = () => {}
+  getActuatorsSetup = () => {
+    return ""
+  }
 
-  getActuatorsLoop = () => {}
+  getActuatorsLoop = () => {
+    return ""
+  }
 
 }
